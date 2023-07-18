87f43f36d7e6772bcc428781cae956ee63194694
Ariel Cabello <080ariel@gmail.com>
2023-07-18 20:30:55 +0200

    Add an entry point with arguments

--- entry.h
+++ entry.h
@@ -1,25 +1,23 @@
 #include "syscalls/syscalls.h"
 
 #ifdef ARGC_ARGV
-
 void __attribute__((naked)) _start(void){
     __asm__ volatile (
-        "movq (%rsp),  %rax ;"
-        "movq 8(%rsp), %rsi ;"
-        "call main         ;"
+        "movq (%rsp),  %rdi ;" // ARGC
+        "leaq 8(%rsp), %rsi ;" // ARGV
+        "call main          ;" // main(argc, argv)
+        "movq %rax, %rdi    ;" // exit()
+        "movq $60,  %rax    ;"
+        "syscall            ;"
     );
-  //      "movq %rsi, %rdi ;"
-  //      "syscall         ;"
-  //      "ret             ;"
 }
-
-//int _start(int argc, char **argv){
-//    main(argc, argv);
-//    exit(0);
-//}
 #else
 int _start(void){
-    main();
-    exit(0); 
+    __asm__ volatile (
+        "call main          ;" // main(argc, argv)
+        "movq %rax, %rdi    ;" // exit()
+        "movq $60,  %rax    ;"
+        "syscall            ;"
+    );
 }
 #endif
