40ad0616c53dea733dfac4a25862905d2aa4330b
Ariel Cabello <080ariel@gmail.com>
2023-11-20 16:03:41 +0100

    Fix mmapfile

--- mmapfile.h
+++ mmapfile.h
@@ -4,25 +4,27 @@
 #ifndef MMAPFILE_H_
 #define MMAPFILE_H_
 // fsfddsf
-char *mmapfile(char *file, int mode){
+char *mmapfile(char *file, int mode, uint64_t file_size){
     int fd;
     struct stat st;
     char *addr;
     
     fd = open(file, mode, 0);
     if(fd < 0){
-        printf("LULY\n");
+        printf("mmapfile(): open failed\n");
+        exit(1);
     }
 
     fstat(fd, &st);
 
     addr = (char *)mmap(NULL, st.st_size, PROT_READ, MAP_SHARED_VALIDATE, fd, 0);
     if(addr < 0){
-        printf("mmap has failed\n");
+        printf("mmapfile(): mmap failed\n");
+        exit(1);
     }
-
     close(fd);
 
+    *file_size = st.st_size;
     return addr;
 }
 
